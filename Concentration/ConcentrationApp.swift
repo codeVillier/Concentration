d170bcc087e0783e4e5e19fb3e7a0e7c5db9fd25
codeVillier <odevill@icloud.com>
2026-01-30 11:11:30 +0200

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Oscar De Villiers on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
